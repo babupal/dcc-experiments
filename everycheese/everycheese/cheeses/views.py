50fbc5ce198e8970487a1c92b3ba0a292ce2119a
Babu Palathingal <babupal@yahoo.com>
2020-07-07 16:56:18 +0530

    Write tests for all cheese views

--- everycheese/everycheese/cheeses/views.py
+++ everycheese/everycheese/cheeses/views.py
@@ -11,9 +11,9 @@ class CheeseDetailView(DetailView):
     model = Cheese
 class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
-    fields = ['name', 'description', 'firmness',
-        'country_of_origin']
+    fields = ['name', 'description', 'firmness','country_of_origin']
 
     def form_valid(self, form):
+        
         form.instance.creator = self.request.user
         return super().form_valid(form)
